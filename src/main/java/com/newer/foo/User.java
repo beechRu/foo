80b0a388b86eea0e5399d9854b4221000cdde508
wtao <wtao@wtao-mac>
2017-08-08 16:21:17 +0800

    实现了 User 模块

---
+++ src/main/java/com/newer/foo/User.java
@@ -0,0 +1,5 @@
+package com.newer.foo;
+
+public class User {
+
+}
---
+++ src/main/java/com/newer/foo/UserController.java
@@ -0,0 +1,8 @@
+package com.newer.foo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
